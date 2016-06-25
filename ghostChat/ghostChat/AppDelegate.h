6afaaf3ee81a0cb82ec98a3fa2d1b2cc801f606b
Seth Miller <sm390709@gmail.com>
2016-06-25 11:33:53 -0700

    first commit for snapchat clone!!! WOOT

---
+++ ghostChat/ghostChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ghostChat
+//
+//  Created by Seth Miller on 6/25/16.
+//  Copyright © 2016 SethMiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
